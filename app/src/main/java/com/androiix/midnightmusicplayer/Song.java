5e6c15af74fb53d587a556574335212999e4adbc
Androiix <kiingusman@gmail.com>
2017-05-25 19:29:38 +0100

    Initial commit

---
+++ app/src/main/java/com/androiix/midnightmusicplayer/Song.java
@@ -0,0 +1,22 @@
+package com.androiix.midnightmusicplayer;
+
+/**
+ * Created by usman on 24/05/2017.
+ */
+public class Song {
+
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist){
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+
+}
